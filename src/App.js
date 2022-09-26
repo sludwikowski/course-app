8fc38be799944c91cc8a94e4db96a0bd42dc84ae
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-09-26 19:12:47 +0200

    Application initial state proposed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react'
 
-export const App = () => {
-  return (
-    <div>
-      Create React App Template
-    </div>
-  )
-}
+export class App extends React.Component {
+  state = {
+    // global state
+    isLoading: false,
+    hasError: false,
+    errorMessage: '',
+    isInfoDisplayed: false,
+    infoMessage: '',
 
+    // user/auth state
+    isUserLoggedIn: false,
+    userDisplayName: '',
+    userEmail: '',
+    userAvatar: '',
+
+    // router state
+    notLoginUserRoute: 'LOGIN', // 'NEW-ACCOUNT' or 'FORGOT PASSWORD'
+
+    // login page state
+    loginEmail: '',
+    loginPassword: '',
+
+    // create account page
+    createAccountEmail: '',
+    createAccountPassword: '',
+    createAccountRepeatPassword: '',
+
+    // recover password page
+    recoverPasswordEmail: '',
+
+    // course list
+    courses: null,
+    searchPhrase: ''
+  }
+
+  render () {
+    return (
+      <div>
+        Course App
+      </div>
+    )
+  }
+}
 export default App
