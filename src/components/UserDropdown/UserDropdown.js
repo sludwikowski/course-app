d69c72418eada0165b622c80a140139dab0181c3
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-10-01 18:09:03 +0200

    `UserDropdown` highlited on hover

--- src/components/UserDropdown/UserDropdown.js
+++ src/components/UserDropdown/UserDropdown.js
@@ -12,6 +12,7 @@ export const UserDropdown = (props) => {
     userDisplayName,
     userEmail,
     userAvatar,
+    contentList,
     ...otherProps
   } = props
 
@@ -21,28 +22,43 @@ export const UserDropdown = (props) => {
       {...otherProps}
     >
       <div
-        className={classes.textWrapper}
+        className={classes.wrapper}
       >
-        <Typography
-          className={classes.userDisplayName}
-          variant={'body1'}
+        <div
+          className={classes.textWrapper}
         >
-          {userDisplayName || '– –'}
-        </Typography>
-        <Typography
-          className={classes.userEmail}
-          variant={'caption'}
+          <Typography
+            className={classes.userDisplayName}
+            variant={'body1'}
+          >
+            {userDisplayName || '– –'}
+          </Typography>
+          <Typography
+            className={classes.userEmail}
+            variant={'caption'}
+          >
+            {userEmail}
+          </Typography>
+        </div>
+        <div
+          className={classes.avatarWrapper}
         >
-          {userEmail}
-        </Typography>
+          <Avatar
+            src={userAvatar}
+          />
+        </div>
       </div>
-      <div
-        className={classes.avatarWrapper}
-      >
-        <Avatar
-          src={userAvatar}
-        />
-      </div>
+
+      {
+        contentList ?
+          <div
+            className={classes.listContainer}
+          >
+            {contentList}
+          </div>
+          :
+          null
+      }
     </div>
   )
 }
@@ -51,7 +67,8 @@ UserDropdown.propTypes = {
   className: PropTypes.string,
   userDisplayName: PropTypes.string,
   userEmail: PropTypes.string,
-  userAvatar: PropTypes.string
+  userAvatar: PropTypes.string,
+  contentList: PropTypes.node
 }
 
 export default UserDropdown
